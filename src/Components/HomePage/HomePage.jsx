cc4903843807f0dc2df673cd51fde81b4003a2b5
Hila Kershenovich <hilawol@gmail.com>
2021-04-08 03:52:39 +0300

    added homepage

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div className="homePageContent">
-
-      <div className="homePageMainGrid">
-
-      </div>
+    <div className="homePageMainGrid">
+      <div className="gridImg1"></div>
+      <div className="gridImg2"></div>
+      <div className="gridImg3"></div>
+      <div className="gridImg4"></div>
+      <div className="gridImg5"></div>
     </div>
   )
 }
