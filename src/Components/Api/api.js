cb49e5f1e91a50db51f3810a45c2c5816da2c232
Hila Kershenovich <hilawol@gmail.com>
2021-04-08 11:17:13 +0300

    added results number

--- src/Components/Api/api.js
+++ src/Components/Api/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
     // apiKey: "b0378c1dd5b845c99438e787a66f28e3",
     // "8c4cf547f295464a9c0b9fe358a906ed"
     //to use:49c9a946e86a4abbb0cf1e502577ecbe
-    number: 1,
+    number: 10,
     instructionsRequired: true,
     addRecipeInformation: true,
     limitLicense: true,
