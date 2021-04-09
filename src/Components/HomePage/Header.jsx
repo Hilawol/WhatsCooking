e428f93bdca4f74a4de7f723790f41c768152e43
Hila Kershenovich <hilawol@gmail.com>
2021-04-09 19:18:21 +0300

    added currentTab to header

--- src/Components/HomePage/Header.jsx
+++ src/Components/HomePage/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-// import logo from '../../Assets/logo.png'
 
 function Header() {
+
+  const [currentTab, setCurrentTab] = useState("home");
+
   return (
     <div className="header">
       <div className="logo" ></div>
-      <Link to="/" className="navBtn">Home</Link>
-      <Link to="/recipes" className="navBtn">Recipes</Link>
+      <Link to="/" className={`navBtn${currentTab === "home" ? ' currentTab' : ''}`} onClick={() => { setCurrentTab("home") }}>Home</Link>
+      <Link to="/recipes" className={`navBtn${currentTab === "recipes" ? ' currentTab' : ''}`} onClick={() => { setCurrentTab("recipes") }}>Recipes</Link>
     </div>
   )
 }
